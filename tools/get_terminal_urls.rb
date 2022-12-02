8b8170cf2a7d44a560464233433bc8346753d993
match8969 <33335788+match8969@users.noreply.github.com>
2022-12-02 12:27:05 +0900

    Update

--- tools/get_terminal_urls.rb
+++ tools/get_terminal_urls.rb
@@ -1,14 +1,8 @@
 # Execute at ruby dir. Like "ruby % ".
 
-
-
 def get_response_of(url)
     require 'net/http'
     require 'net/https'
-
-    puts "aaaaaaa"
-
-    puts ""
 
     begin
         uri = URI.parse(url)
@@ -28,11 +22,7 @@ def get_terminal_url(url)
 
     res = get_response_of(url)
 
-    puts "bbbbbbb"
-
     return url if res.nil?
-
-    puts "res.code: #{res.code}"
 
     # 最大5回までリダイレクト先URLを探索する (RFC 1945)
     5.times do
@@ -41,16 +31,9 @@ def get_terminal_url(url)
         # リダイレクト先URLを設定
         redirected_url = res[:Location]
 
-        puts "redirect_url : #{redirected_url}"
-
         unless redirected_url.start_with?(/http/)
             uri  = URI.parse(url)
-
-            prefix = "#{uri.scheme}://#{uri.host}" # => "http://"
-
-            puts "prefix: #{prefix}"
-            puts "redirect_url: #{redirected_url}"
-
+            prefix = "#{uri.scheme}://#{uri.host}"
             redirected_url = prefix + redirected_url
         end
 
@@ -62,36 +45,42 @@ def get_terminal_url(url)
     end
 
     url
-
 end
 
 def main
     require "csv"
-    # require "addressable/uri"
     require 'uri'
 
-
-    # input_file_name = "web_search_result_test.csv"
-    input_file_name = "a8_4c_short.csv"
+    # input_file_name = "a8_4c_more_short.csv"
+    input_file_name = "a8_4c.csv"
     input_csv_path = "#{Dir.getwd}/data/csv/#{input_file_name}"
 
-    # puts "#{input_csv_path}"
 
-    # data_list = CSV.read(input_csv_path)
-    # data_list.each do |url|
-    #     puts "aaa"
+    # CSV読み込み
+    data_list = CSV.read(input_csv_path)
+    
+    # CSV生成
+    output_file_name = "#{Time.now.strftime("%Y%m%d_%H%M%S")}.csv"
+    output_file_path = "#{Dir.getwd}/output/csv/#{output_file_name}"
+    CSV.open(output_file_path, "wb") do |csv|
+        csv << [  "元URL", "たどり着くURL"]
 
-    # end
-    # p data_list
+        data_list.each do |list|
 
-    # url = "https://px.a8.net/svt/ejp?a8mat=3NCU0O+4BZL9U+4OAC+BW0YB&a8ejpredirect=https%3A%2F%2Fwww.tigerbottles.com%2Fproducts%2Fmxv%3Foptions%3D4779924750473%252C500mL%252C39647231148169"
+            url = list[0]
+            unless url.start_with?(/http/)
+                next
+            end
 
-    # url = "https://www.tigerbottles.com/products/mxv?options=4779924750473%2C500mL%2C39647231148169&a8=3k7Hqk8V-OzU0_7PD5pnSBZ9U_cjz7za05z48OaTS46V-O7kRJ7ZzJ0Q91Slfl0z94g1TD7Z9k7HFs00000021810001"
-    # url = "https://store.tiger-corporation.com/products/mxv?options=4779924750473%2C500mL%2C39647231148169&a8=3k7Hqk8V-OzU0_7PD5pnSBZ9U_cjz7za05z48OaTS46V-O7kRJ7ZzJ0Q91Slfl0z94g1TD7Z9k7HFs00000021810001"
-    url = "https://store.tiger-corporation.com/products/mxv-e?a8=3k7Hqk8V-OzU0_7PD5pnSBZ9U_cjz7za05z48OaTS46V-O7kRJ7ZzJ0Q91Slfl0z94g1TD7Z9k7HFs00000021810001&options=4779924750473%2C500mL%2C39647231148169"
-    terminal_url = get_terminal_url(url)
+            terminal_url = get_terminal_url(url)
+            csv << [ "#{url}", "#{terminal_url}"]
+        end
+    end
 
-    puts "main(): terminal_url : #{terminal_url}"
+
+    puts "かんりょう"
+    puts "open #{output_file_path}"
+    
 end
 
 main()
